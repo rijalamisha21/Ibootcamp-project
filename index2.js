84c54d070525d64310988011f4f1d1274815f599
rijalamisha21 <rijalamisha17@gmail.com>
2022-04-02 19:26:14 +0545

    icon-margin changed

---
+++ index2.js
@@ -0,0 +1,23 @@
+      var slide = document.getElementById("images");
+      var btn1 = document.getElementById(" btn1");
+      var btn2 = document.getElementById(" btn2");
+      var btn3 = document.getElementById(" btn3");
+
+      btn1.onclick = function () {
+        slide.style.transform = " translateX(0px)";
+        btn1.classList.add("active");
+        btn1.classList.remove("active");
+      }
+
+      btn2.onclick = function () {
+        slide.style.transform = " translateX(-100%)";
+        btn2.classList.add("active");
+        btn2.classList.remove("active");
+      }
+
+      btn3.onclick = function () {
+        slide.style.transform = " translateX(-200%)";
+        btn3.classList.add("active");
+        btn3.classList.remove("active");
+        
+      }
